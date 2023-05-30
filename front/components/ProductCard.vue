638d4903ffc1a06ebb5b7a982ac2361ff294e664
nitesh-dev <niteshdev547@gmail.com>
2023-05-30 16:57:26 +0530

    image size changed

--- front/components/ProductCard.vue
+++ front/components/ProductCard.vue
@@ -29,7 +29,7 @@ function urlToLandingUrl(url: string) {
     const lastIndex = url.lastIndexOf('.')
     const left = url.slice(0, lastIndex)
     const right = url.slice(lastIndex + 1, url.length)
-    const modifiedUrl = left + "._SX522_." + right
+    const modifiedUrl = left + "._SX480_." + right
     console.log(modifiedUrl)
     return modifiedUrl
 }
